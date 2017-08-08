08fa0ad77e52dcce32898cef222c034db2d3c536
Michael Wrock <Michael.Wrock@cusolutionsgroup.com>
2017-08-07 23:11:40 -0400

    Document Types

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,6 +23,7 @@ export class HomeComponent implements OnInit {
       qId: '',
       qType: 'FieldList'
     },
+    qExtendsId: '',
     qFieldListDef: {
       qShowSystem: false,
       qShowHidden: false,
